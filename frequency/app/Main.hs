88c55dd352a4cdd6d1e43f01a904f30e5e3fe38e
sarakhandaker <kttsara@gmail.com>
2021-08-19 11:02:00 -0700

    new project for frequency calc

---
+++ frequency/app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Lib
+import Control.Applicative
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+import Data.Time
+
+main :: IO ()
+main = print "db-lesson"
+
+
